0f299938df3b832a573a2965742f491f214eeb3d
Mariajesús Bucio Villaseñor <buciovillasenormariajesus@gmail.com>
2026-06-19 00:21:06 -0600

    Modificaciones a HTML, CSS, PHP, solo falta la configuración de la parte de diseño responsivo, busquedas con GET y CON POST

---
+++ Tarea_2.3.1/Tarea_2.3.1/Tarea_2/consulta_post_pdo.php
@@ -0,0 +1,33 @@
+<?php
+try {
+    $dsn = "pgsql:host=localhost;port=5432;dbname=siicyt;";
+    $user = "postgres";
+    $password = "ubuntu";
+
+    $pdo = new PDO("$dsn", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    
+    // Recibimos tus nuevas variables
+    $palabra = $_POST['palabra'] ?? '';
+    $entidad = $_POST['entidad'] ?? '';
+
+    // Nueva consulta combinada (Inciso F)
+    $query = "SELECT nombre, tipo, delmun, entidad, sector, rama, clase FROM reniecyt2013a 
+              WHERE nombre ILIKE :palabra AND entidad = :entidad 
+              ORDER BY nombre LIMIT 50";
+ 
+    $stmt = $pdo->prepare($query);
+    // Ejecutamos inyectando los comodines para la búsqueda de palabra
+    $stmt->execute([
+        ':palabra' => '%' . $palabra . '%',
+        ':entidad' => $entidad
+    ]);
+
+    $data = $stmt->fetchAll();
+    echo json_encode($data);
+} catch (PDOException $e) {
+    die ("Error de conexión: " . $e->getMessage());
+}
+?>
